cab7107dd10bfdc906e9740a001d43362d11b36f
rom4a <aroundthefog@gmail.com>
2024-09-16 18:18:55 +0400

    Added data filtering

--- bot.py
+++ bot.py
@@ -1,26 +1,43 @@
 import os
 from dotenv import load_dotenv
 import telebot
+import requests
 
-from modules import get_rates
+# from modules import get_rates
 
 load_dotenv()
 
 
 token = os.getenv('TOKEN')
 bot = telebot.TeleBot(token)
+api_login = os.getenv('API_LOGIN')
+api_key = os.getenv('API_KEY')
+base_url = os.getenv('BASE_URL')
+method_name = 'get_directions'
+endpoint = base_url + method_name
+query_headers = {'API-LOGIN': api_login, 'API-KEY': api_key}
 
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_message(message):
     bot.send_message(message.chat.id, message.text)
     print(message.text)
+    print(message.chat.id)
+
+response = requests.post(endpoint, headers=query_headers)
+data = response.json().get('data')
+
+
+def get_give_id(filter_name, filter_item, data_list):
+    filtered_give_id = [item for item in data_list if item[filter_item] == str(filter_name)]
+    return filtered_give_id
+
+
+result_get_id = get_give_id(568, 'currency_give_id', data)
+print(result_get_id) #получаем все пары с использованием "Наличные" USD
+
 
 
 if __name__ == '__main__':
     bot.infinity_polling()
 
-get_rates('usd', 'usdt', 'MSK', 1239)
-get_rates('usd', 'usdt', 'LA', 3123)
-get_rates('usd', 'usdt', 'TLT', 25)
-get_rates('usd', 'usdt', 'NY', 123123)
--- modules.py
+++ modules.py
@@ -1,5 +1,6 @@
 from random import randint
 
 
-def get_rates(buy, sell, location, amount):
-    return print(f"Отдаю {sell}, получаю {buy}, город {location} колличество {amount}. Rate: {randint(1, 10)}")
+# def get_rates(buy, sell, location, amount):
+#     return f"Отдаю {sell}, получаю {buy}, город {location} колличество {amount}. Rate: {randint(1, 10)}"
+
