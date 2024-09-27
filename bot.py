a7c89909892891fc78d2a5344921d05a2c0ca91d
rom4a <aroundthefog@gmail.com>
2024-09-27 06:36:48 +0400

    feat!: add Enum class in new file currencies.py. delete func get_direction_data style: rename variables give_id > currency_in, get_id > currency_out. Make commands arr,

--- bot.py
+++ bot.py
@@ -5,51 +5,55 @@ from dotenv import load_dotenv
 from exchange_api import get_all_exchange_rate as exchange_rate
 from exchange_api import get_chosen_city_name as chosen_city_name
 from template_text import header_message, rub_to_usd, usd_to_rub, footer_text
-
+from currencies import Currencies
 
 load_dotenv()
 token = os.getenv('TOKEN')
 bot = telebot.TeleBot(token)
-
-
-@bot.message_handler(commands=['help', 'start'])
-def send_welcome(message):
-    bot.send_message(message.chat.id, '/rate_LOSANGELES\n\n'
-                                      '/rate_MIAMI\n\n'
-                                      '/rate_NEWYORK\n\n'
-                                      '/rate_CHICAGO')
-
-
-@bot.message_handler(commands=[
+commands = [
     "rate_LOSANGELES",
     "rate_MIAMI",
     "rate_NEWYORK",
     "rate_CHICAGO"
-])
+]
+
+
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    bot.send_message(message.chat.id, f'\n/{commands[0]}\n'
+                                      f'\n/{commands[1]}\n'
+                                      f'\n/{commands[2]}\n'
+                                      f'\n/{commands[3]}\n'
+                     )
+
+
+@bot.message_handler(commands=commands)
 def send_rate(message):
     bot.send_message(message.chat.id, 'Обработка запроса 🌚')
     city = message.text[6:]
-    rub_to_cash = exchange_rate('WIRERUB', 'CASHUSD', city, 1)
-    rub_to_zelle = exchange_rate('WIRERUB', 'ZELLE', city, 1)
-    cash_usd_to_rub = exchange_rate('CASHUSD', 'WIRERUB', city, 1)
-    zelle_to_rub = exchange_rate('ZELLE', 'WIRERUB', city, 1)
-    
-    bot.send_message(message.chat.id, f'{header_message}\n\n'
-                                      f'Курс для города {chosen_city_name(city)}:\n\n'
-                                      f'{rub_to_usd}:\n'
-                                      f'\n{rub_to_cash[0]} / {rub_to_zelle[0]} от 10000₽'
-                                      f'\n{rub_to_cash[1]} / {rub_to_zelle[1]} от 50000₽'
-                                      f'\n{rub_to_cash[2]} / {rub_to_zelle[2]} от 150.000₽'
-                                      f'\n{rub_to_cash[3]} / {rub_to_zelle[3]} от 500.000₽'
-                                      f'\n{rub_to_cash[4]} / {rub_to_zelle[4]} от 1 млн ₽'
-                                      f'\n\n{usd_to_rub}'
-                                      f'\n{cash_usd_to_rub[0]} / {zelle_to_rub[0]} от 100$'
-                                      f'\n{cash_usd_to_rub[1]} / {zelle_to_rub[1]} от 500$'
-                                      f'\n{cash_usd_to_rub[2]} / {zelle_to_rub[2]} от 1500$'
-                                      f'\n{cash_usd_to_rub[3]} / {zelle_to_rub[3]} от 5000$'
-                                      f'\n{cash_usd_to_rub[4]} / {zelle_to_rub[4]} от 10000$'
-                                      f'\n\n{footer_text}'
-                     )
+    rub_to_cash = exchange_rate(Currencies.WIRERUB.value, Currencies.CASHUSD.value, city)
+    rub_to_zelle = exchange_rate(Currencies.WIRERUB.value, Currencies.ZELLE.value, city)
+    cash_usd_to_rub = exchange_rate(Currencies.CASHUSD.value, Currencies.WIRERUB.value, city)
+    zelle_to_rub = exchange_rate(Currencies.ZELLE.value, Currencies.WIRERUB.value, city)
+    if rub_to_cash and rub_to_zelle and cash_usd_to_rub and zelle_to_rub:
+        bot.send_message(message.chat.id, f'{header_message}\n\n'
+                                          f'Курс для города {chosen_city_name(city)}:'
+                                          f'\n\n{rub_to_usd}:\n'
+                                          f'\n{rub_to_cash[0]} / {rub_to_zelle[0]} от 10000₽'
+                                          f'\n{rub_to_cash[1]} / {rub_to_zelle[1]} от 50000₽'
+                                          f'\n{rub_to_cash[2]} / {rub_to_zelle[2]} от 150.000₽'
+                                          f'\n{rub_to_cash[3]} / {rub_to_zelle[3]} от 500.000₽'
+                                          f'\n{rub_to_cash[4]} / {rub_to_zelle[4]} от 1 млн ₽'
+                                          f'\n\n{usd_to_rub}\n'
+                                          f'\n{cash_usd_to_rub[0]} / {zelle_to_rub[0]} от 100$'
+                                          f'\n{cash_usd_to_rub[1]} / {zelle_to_rub[1]} от 500$'
+                                          f'\n{cash_usd_to_rub[2]} / {zelle_to_rub[2]} от 1500$'
+                                          f'\n{cash_usd_to_rub[3]} / {zelle_to_rub[3]} от 5000$'
+                                          f'\n{cash_usd_to_rub[4]} / {zelle_to_rub[4]} от 10000$'
+                                          f'\n\n{footer_text}'
+                         )
+    else:
+        bot.send_message(message.chat.id, 'sorry is None')
 
 
 @bot.message_handler(content_types=['text'])
--- exchange_api.py
+++ exchange_api.py
@@ -10,67 +10,45 @@ api_key = os.getenv('API_KEY')
 base_url = os.getenv('BASE_URL')
 query_headers = {'API-LOGIN': api_login, 'API-KEY': api_key}
 
-currency_id_to_name_mapping = {
-    "CASHUSD": "568",
-    "ETH": "355",
-    "WIREUAH": "537",
-    "ZELLE": "578",
-    "WIREKZT": "539",
-    "USDTBEP20": "369",
-    "USDTERC20": "367",
-    "TRX": "385",
-    "WIRERUB": "535",
-    "LYUBOYBANK": "581",
-    "USDCERC20": "371",
-    "USDTTRC20": "368",
-    "BTC": "348"
-}
 
-
-def get_direction(give_id, get_id):
+def get_direction(currency_out, currency_in):
     # send api request get_directions with params
 
-    give_id = str(currency_id_to_name_mapping.get(give_id, f'{give_id}'))
-    get_id = str(currency_id_to_name_mapping.get(get_id, f'{get_id}'))
     method_name = 'get_directions'
     endpoint = base_url + method_name
     response = requests.post(endpoint, headers=query_headers)
     directions = response.json().get('data')
     for direction in directions:
-        if direction.get('currency_give_id') == str(give_id) and direction.get('currency_get_id') == str(get_id):
-            return str(direction.get('direction_id'))
+        
+        if (direction.get('currency_give_id') == currency_out
+                and direction.get('currency_get_id') == currency_in):
+            return direction.get('direction_id')
 
 
-def get_direction_data(direction_id):
-    # send api request get_direction with params
-    method_name = 'get_direction'
-    endpoint = base_url + method_name
-    response = requests.post(endpoint, data={'direction_id': str(direction_id)}, headers=query_headers)
-    direction_data = response.json().get('data')
-
-    return direction_data
-
-
-def get_calc(direction_id, amount, action, city):
+def get_calc(direction_id, amount, city, action=1):
     # send api request get_calc with params
+
     method_name = 'get_calc'
     endpoint = base_url + method_name
-    response = requests.post(endpoint,
-                             data={
-                                 'direction_id': str(direction_id),
-                                 'calc_amount': amount,
-                                 'calc_action': action,
-                                 'cd': f'city={city}'
-                             },
-                             headers=query_headers
-                             )
+    data = {
+        'direction_id': str(direction_id),
+        'calc_amount': amount,
+        'calc_action': action,
+        'cd': f'city={city}'
+        }
+    response = requests.post(
+        endpoint,
+        data=data,
+        headers=query_headers
+    )
     request_data = response.json().get('data', {})
     course_give = request_data.get('course_give')
+    course_get = request_data.get('course_get')
 
-    if course_give == '1':
-        key = 'course_get'
-    else:
+    if course_give > course_get:
         key = 'course_give'
+    else:
+        key = 'course_get'
     exchange_rate = request_data.get(key)
 
     return exchange_rate
@@ -80,47 +58,46 @@ def get_chosen_city_name(city):
     # select city name from api
     method_name = 'get_direction'
     endpoint = base_url + method_name
-    response = requests.post(endpoint, data={'direction_id': '1331'}, headers=query_headers)
-    city_name = response.json().get("data", {}).get("dir_fields", {}).get("city", {}).get("options", {}).get(city, "0")
-    return city_name
+    example_direction_id = '1331'
+    data = {'direction_id': example_direction_id}
+    response = requests.post(endpoint, data=data, headers=query_headers)
+    city = (
+        response.json().get("data", {}).get("dir_fields", {}).get("city", {}).get("options", {}).get(city, "Не выбран")
+    )
+
+    return city
 
 
-def get_exchange_rate_for_currency_pair(give_id, get_id, city, amount, action):
+def get_exchange_rate_for_currency_pair(currency_out, currency_in, city, amount, action=1):
     # get exchange rate
 
-    give_id = str(currency_id_to_name_mapping.get(give_id, f'{give_id}'))
-    get_id = str(currency_id_to_name_mapping.get(get_id, f'{get_id}'))
-
-    direction_id = get_direction(give_id=str(give_id), get_id=str(get_id))
+    direction_id = get_direction(currency_out=currency_out, currency_in=currency_in)
 
     if not direction_id:
         return None
 
-    direction_data = get_direction_data(direction_id=direction_id)
-
-    if not direction_data:
-        return None
-
-    exchange_rate = get_calc(direction_id, amount, action, city)
+    exchange_rate = get_calc(direction_id, amount, city, action)
 
     return exchange_rate
 
 
-def get_all_exchange_rate(give_id, get_id, city, action):
+def get_all_exchange_rate(currency_out, currency_in, city):
     # get all available procedures depending on the amount
 
-    give_id = str(currency_id_to_name_mapping.get(give_id, f'{give_id}'))
-    get_id = str(currency_id_to_name_mapping.get(get_id, f'{get_id}'))
+    rub_id = '535'
     amounts_list = {
         'USD': [100, 500, 1500, 5000, 10000],
         'RUB': [10000, 50000, 150000, 500000, 1000000]
     }
 
     amounts = amounts_list.get('USD')
-    if str(give_id) == '535':
+    if currency_out == rub_id:
         amounts = amounts_list.get('RUB')
 
     rate_arr = []
     for summ in amounts:
-        rate_arr += [get_exchange_rate_for_currency_pair(give_id, get_id, city, summ, action)]
+        exchange_rate = get_exchange_rate_for_currency_pair(currency_out, currency_in, city, summ, action=1)
+        if exchange_rate is None:
+            return None
+        rate_arr += [exchange_rate]
     return rate_arr
